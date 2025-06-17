90c75057373e0b3249f6cf8f848a4a819dd3a4ea
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-06-17 22:31:47 +0200

    change header and structure of routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import Loader from "./components/Loader/Loader.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,25 +1,29 @@
 import LoginRegisterForm from "../../components/LoginRegisterForm/LoginRegisterForm";
 import illustration from "../../assets/images/illustration.png";
 import css from "../LoginPage/LoginPage.module.css";
+import Logo from "../../components/Logo/Logo";
 
 const RegisterPage = () => {
   return (
-    <div className={css.container}>
-      <LoginRegisterForm operation={"Register"} />
-      <div className={css.imageWrap}>
-        <img src={illustration} alt="illustration" className={css.image} />
+    <>
+      <Logo />
+      <div className={css.container}>
+        <LoginRegisterForm operation={"Register"} />
+        <div className={css.imageWrap}>
+          <img src={illustration} alt="illustration" className={css.image} />
 
-        <ul className={css.list}>
-          <li>Word</li>
-          <li>·</li>
-          <li>Translation</li>
-          <li>·</li>
-          <li>Grammar</li>
-          <li>·</li>
-          <li>Progress</li>
-        </ul>
+          <ul className={css.list}>
+            <li>Word</li>
+            <li>·</li>
+            <li>Translation</li>
+            <li>·</li>
+            <li>Grammar</li>
+            <li>·</li>
+            <li>Progress</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
