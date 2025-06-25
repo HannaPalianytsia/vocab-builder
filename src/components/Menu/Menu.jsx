975f69936b6c9662fcdde688429d534c5a455e2a
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-06-25 20:50:09 +0200

    add styles to menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,22 +1,38 @@
 import { NavLink } from "react-router-dom";
 import css from "./Menu.module.css";
+import clsx from "clsx";
 import illustration from "../../assets/images/illustration.png";
 import Icon from "../Icon";
 
-const Menu = ({ name, buildLinkClass }) => {
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
+const Menu = ({ name }) => {
   return (
     <div className={css.menu}>
-      <div className={css.user}>
-        <p className={css.username}>{name}</p>
-        <div className={css.usericon}>
-          <Icon
-            id={"icon-user"}
-            width={14}
-            height={14}
-            className={"user-svg"}
-            fillColor={"rgba(252, 252, 252, 0.7)"}
-          />
+      <div className={css.menuheader}>
+        <div className={css.user}>
+          <p className={css.username}>{name}</p>
+          <div className={css.usericon}>
+            <Icon
+              id={"icon-user"}
+              width={14}
+              height={14}
+              className={"user-svg"}
+              fillColor={"var(--main-color)"}
+            />
+          </div>
         </div>
+        <button className={css.closebutton}>
+          <Icon
+            id={"icon-close"}
+            width={20}
+            height={20}
+            className={"close-svg"}
+            strokeColor={"var(--background-color-2)"}
+          />
+        </button>
       </div>
 
       <nav className={css.nav}>
@@ -31,11 +47,15 @@ const Menu = ({ name, buildLinkClass }) => {
         <NavLink to="/training" className={buildLinkClass}>
           Training
         </NavLink>
+
+        <button type="button" className={clsx(css.link, css.logoutBtn)}>
+          Log out
+        </button>
       </nav>
 
-      <button type="button">Log out</button>
-
-      <img src={illustration} alt="illustration" />
+      <div className={css.illustration}>
+        <img src={illustration} alt="illustration" />
+      </div>
     </div>
   );
 };
