243ca91727be08e3f903657886453dedc6aecb3a
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2025-08-26 18:03:11 +0300

    Update DictionaryPage.jsx

--- src/pages/DictionaryPage/DictionaryPage.jsx
+++ src/pages/DictionaryPage/DictionaryPage.jsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { wordsCategories } from "../../redux/word/operations";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllWords, wordsCategories } from "../../redux/word/operations";
 import Dashboard from "../../components/Dashboard/Dashboard";
+import { selectWords } from "../../redux/word/selectors";
 
 const DictionaryPage = () => {
   const dispatch = useDispatch();
 
+  const words = useSelector(selectWords);
+  console.log(words);
+
   useEffect(() => {
     dispatch(wordsCategories());
+    dispatch(fetchAllWords());
   }, [dispatch]);
 
   return (
